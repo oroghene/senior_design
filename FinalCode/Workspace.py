b47ae86c0a3b6d8a0a8815d9cfb0d786818dbb18
ZiadHatab17 <54187297+ZiadHatab17@users.noreply.github.com>
2023-05-08 13:18:56 -0400

    ...

--- FinalCode/Workspace.py
+++ FinalCode/Workspace.py
@@ -1,17 +1,15 @@
 
 import csv
 
-with open('Points.csv') as csv_file:
+# Import csv and create list of Lidar points
+Points = []
+
+with open('Scans.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+        Points.append([float(row[0]),float(row[1])])
+    print(Points)
 
 def find_furthest_distance(points):
     paths = []
